d95e2ac0808d2a68f803e337aa9da8876068254a
noman27 <blt.normanbruce@yahoo.com>
2023-06-03 17:28:23 +0600

    slider needs some fix

--- src/Pages/Home/HomeBanner/HomeBanner.js
+++ src/Pages/Home/HomeBanner/HomeBanner.js
@@ -1,14 +1,44 @@
-import React from "react";
+import { useState } from "react";
 import one from "../../../assets/HomeImages/1.jpg";
 import two from "../../../assets/HomeImages/2.jpg";
 import three from "../../../assets/HomeImages/3.jpg";
 import checkoutLogo from "../../../assets/HomeImages/Checkout-white.png";
 import "../home.css";
 import { Link } from "react-router-dom";
-import currentSlide from "./BannerSlider";
 
 const HomeBanner = () => {
-  //onClick={() => currentSlide(1)}
+  const [slideIndex, setSlideIndex] = useState(1);
+
+  const mock = (n) => {
+    setSlideIndex(n);
+    let slides = document
+      .getElementsByClassName("slider")[0]
+      .getElementsByTagName("img");
+    let dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) {
+      setSlideIndex(1);
+    }
+    if (n < 1) {
+      setSlideIndex(slides.length);
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+      console.log(slideIndex);
+      slides[i].style.transform =
+        "translateX(-" + (slideIndex - 1) * 100 + "%)";
+      slides[i].style.transition = "transform 0.8s ease-in-out"; // Add transition property for smooth animation
+    }
+
+    for (let i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", ""); // Remove active class from all dots
+      dots[i].style.backgroundColor = ""; // Remove black color from all dots
+    }
+
+    dots[slideIndex - 1].className += " active"; // Add active class to current dot
+    dots[slideIndex - 1].style.backgroundColor = "black"; // Add black color to current dot
+  };
+
   return (
     <header>
       <div className="slider-container">
@@ -18,9 +48,9 @@ const HomeBanner = () => {
           <img src={three} alt="" />
         </div>
         <div className="dots-container">
-          <span className="dot" onClick={() => currentSlide(1)}></span>
-          <span className="dot" onClick={() => currentSlide(2)}></span>
-          <span className="dot" onClick={() => currentSlide(3)}></span>
+          <span className="dot" onClick={() => mock(1)}></span>
+          <span className="dot" onClick={() => mock(2)}></span>
+          <span className="dot" onClick={() => mock(3)}></span>
         </div>
       </div>
 
---
+++ src/utility/utils.js
@@ -0,0 +1,160 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+export default function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let slides = document
+    .getElementsByClassName("slider")[0]
+    .getElementsByTagName("img");
+  let dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.transform = "translateX(-" + (slideIndex - 1) * 100 + "%)";
+    slides[i].style.transition = "transform 0.8s ease-in-out"; // Add transition property for smooth animation
+  }
+
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", ""); // Remove active class from all dots
+    dots[i].style.backgroundColor = ""; // Remove black color from all dots
+  }
+
+  dots[slideIndex - 1].className += " active"; // Add active class to current dot
+  dots[slideIndex - 1].style.backgroundColor = "black"; // Add black color to current dot
+}
+
+///pricerange
+
+const rangeInput = document.querySelectorAll(".range-input input"),
+  priceInput = document.querySelectorAll(".price-input input"),
+  range = document.querySelector(".sliderr .progress");
+let priceGap = 1000;
+
+priceInput.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    let minPrice = parseInt(priceInput[0].value),
+      maxPrice = parseInt(priceInput[1].value);
+
+    if (maxPrice - minPrice >= priceGap && maxPrice <= rangeInput[1].max) {
+      if (e.target.className === "input-min") {
+        rangeInput[0].value = minPrice;
+        range.style.left = (minPrice / rangeInput[0].max) * 100 + "%";
+      } else {
+        rangeInput[1].value = maxPrice;
+        range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
+      }
+    }
+  });
+});
+
+rangeInput.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    let minVal = parseInt(rangeInput[0].value),
+      maxVal = parseInt(rangeInput[1].value);
+
+    if (maxVal - minVal < priceGap) {
+      if (e.target.className === "range-min") {
+        rangeInput[0].value = maxVal - priceGap;
+      } else {
+        rangeInput[1].value = minVal + priceGap;
+      }
+    } else {
+      priceInput[0].value = minVal;
+      priceInput[1].value = maxVal;
+      range.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+      range.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+    }
+  });
+});
+
+const tagInput = document.querySelector(".tag-input");
+const tagArea = document.querySelector(".tag-area");
+const ul = document.querySelector(".tag-area ul");
+const label = document.querySelector(".label");
+
+let tags = [];
+
+function addEvent(element) {
+  tagArea.addEventListener("click", () => {
+    element.focus();
+  });
+
+  element.addEventListener("focus", () => {
+    tagArea.classList.add("active");
+    label.classList.add("label-active");
+  });
+
+  element.addEventListener("blur", (e) => {
+    tagArea.classList.remove("active");
+    if (element.value === "" && tags.length === 0) {
+      label.classList.remove("label-active");
+    }
+    if (!element.value.match(/^\s+$/gi) && element.value !== "") {
+      tags.push(e.target.value.trim());
+      element.value = "";
+      renderTags();
+    }
+  });
+
+  element.addEventListener("keydown", (e) => {
+    console.log(e);
+    const value = e.target.value;
+    if (
+      (e.keyCode === 32 ||
+        e.keyCode === 13 ||
+        value[value.length - 1] === " ") &&
+      !value.match(/^\s+$/gi) &&
+      value !== ""
+    ) {
+      tags.push(e.target.value.trim());
+      element.value = "";
+      renderTags();
+    }
+    if (e.keyCode === 8 && value === "") {
+      tags.pop();
+      renderTags();
+    }
+  });
+}
+
+// tag input
+addEvent(tagInput);
+
+function renderTags() {
+  ul.innerHTML = "";
+  tags.forEach((tag, index) => {
+    createTag(tag, index);
+  });
+  const input = document.createElement("input");
+  input.type = "text";
+  input.className = "tag-input";
+  addEvent(input);
+  ul.appendChild(input);
+  input.focus();
+  setTimeout(() => (input.value = ""), 0);
+}
+
+function createTag(tag, index) {
+  const li = document.createElement("li");
+  li.className = "tag";
+  const text = document.createTextNode(tag);
+  const span = document.createElement("span");
+  span.className = "cross";
+  span.dataset.index = index;
+  span.addEventListener("click", (e) => {
+    tags = tags.filter((_, index) => index !== e.target.dataset.index);
+    renderTags();
+  });
+  li.appendChild(text);
+  li.appendChild(span);
+  ul.appendChild(li);
+}
