1dc1a825b31839f1a7fa8c5063591aa5699a1dcf
noman27 <blt.normanbruce@yahoo.com>
2023-06-28 22:35:10 +0600

    Navbar animation active problem

--- src/Pages/RestaurantPageComps/RestNavBar/RestNavBar.js
+++ src/Pages/RestaurantPageComps/RestNavBar/RestNavBar.js
@@ -16,7 +16,23 @@ const RestNavBar = () => {
           <RadioButtonOptions></RadioButtonOptions>
         </div>
       </div>
-      <div className=" flex flex-row"></div>
+      <div className=" text-lg font-semibold  ms-36 flex flex-row">
+        <button className=" mr-12  hover:text-deepRed hover:transition duration-150 ease-in-out hover:border-b-4">
+          Feed
+        </button>
+        <button className=" mr-12  hover:text-deepRed hover:transition duration-150 ease-in-out hover:border-b-4">
+          About
+        </button>
+        <button className=" mr-12  hover:text-deepRed hover:transition duration-150 ease-in-out hover:border-b-4">
+          Menu
+        </button>
+        <button className=" mr-12  hover:text-deepRed hover:transition duration-150 ease-in-out hover:border-b-4">
+          Photos
+        </button>
+        <button className=" mr-12  hover:text-deepRed hover:transition duration-150 ease-in-out hover:border-b-4">
+          Reviews
+        </button>
+      </div>
     </div>
   );
 };
