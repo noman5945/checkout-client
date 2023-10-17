4211c8120c9690317c0c75e277ec7d2e2648d157
noman5945 <abdullahnoman.5945@gmail.com>
2023-10-17 13:50:06 +0600

    resurent list some works left

--- src/Pages/RestaurantList/Restaurant.js
+++ src/Pages/RestaurantList/Restaurant.js
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from "react";
 import RestaurantListCard from "./RestaurantListCard/RestaurantListCard";
 import { Link } from "react-router-dom";
 import RestaurantFilter from "./RestaurantFilter/RestaurantFilter";
+import "../Home/home.css";
+import restLogo from "../../assets/BackGroundImages/Checkout-Logowhite_2.png";
+import restBackGround from "../../assets/BackGroundImages/RestBackGround.png";
+import "./Restaurant.css";
 
 const Restaurant = () => {
   const [restaurants, setRestaurants] = useState([]);
@@ -25,19 +29,36 @@ const Restaurant = () => {
       .then((data) => setRestaurants(data));
   }, []);
   return (
-    <div className="flex flex-row justify-center items-start">
-      <div>
-        <RestaurantFilter></RestaurantFilter>
+    <div className=" relative">
+      <div className=" absolute left-[122px]">
+        <img className="RestBackground" src={restBackGround} alt="" />
       </div>
-      <div>
-        {restaurants.map((restaurant) => (
-          <Link key={restaurant._id} to={"/restaurant"}>
-            <RestaurantListCard
-              resturant={restaurant}
-              time={time}
-            ></RestaurantListCard>
-          </Link>
-        ))}
+      <div className=" mt-[98px] absolute  left-[190px]">
+        <div className=" flex flex-row justify-center items-center">
+          <div className=" mr-[90px]">
+            <img src={restLogo} alt="checkout" />
+          </div>
+          <div>
+            <div className="search-box">
+              <input type="text" placeholder="Search..." />
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-row justify-center items-start">
+          <div>
+            <RestaurantFilter></RestaurantFilter>
+          </div>
+          <div>
+            {restaurants.map((restaurant) => (
+              <Link key={restaurant._id} to={"/restaurant"}>
+                <RestaurantListCard
+                  resturant={restaurant}
+                  time={time}
+                ></RestaurantListCard>
+              </Link>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
